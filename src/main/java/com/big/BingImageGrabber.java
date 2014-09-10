40203df8f2b6ef7bfc7b9ea2aff9c25ff579cf44
Tyler Barber <tylerwbarber@gmail.com>
2014-09-09 21:07:46 -0500

    Created Main class to start app from jar

--- src/main/java/com/big/BingImageGrabber.java
+++ src/main/java/com/big/BingImageGrabber.java
@@ -1,35 +1,27 @@
 package com.big;
 /* 
 	Bing Image Grabber - Version 0.2.1
-
-	Main class. Gets BingID from user and makes objects.
-
-	TODO: 
-		- Make iterations to run multiple querys
  */
-
-import java.io.File;
-import java.util.Scanner;
 
 public class BingImageGrabber {
 
-	// Sets default values of content filter to Moderate, and images to 30
-	// If custom search is chosen, user enters search term, and chooses other parameters
-	public static void main(String[] args) throws Exception {
-
-		String adult = "Moderate";
-		int numberImages = -1;
-		
-		
+	private String adult;
+	private int numberImages;
+	
+	public BingImageGrabber() {
+		this.adult = "Moderate";
+		this.numberImages = -1;
+	}
+	
+	public void runBingImageGrabber() {
 		System.out.println("\nBing Image Grabber 0.2.1\n");
 
 		Menu mainMenu = new Menu();
 		mainMenu.firstRun();
-		
 		String queryChoice = mainMenu.queryMenu();
 		
 		if(queryChoice.equalsIgnoreCase("random"))
-			numberImages = 30;
+			this.numberImages = 30;
 		else{
 			adult = mainMenu.filterMenu();
 			numberImages = mainMenu.countMenu();
---
+++ src/main/java/com/big/Main.java
@@ -0,0 +1,9 @@
+package com.big;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
