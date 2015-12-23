f808c2b97316611b6b76275f7599f563573f5dd1
twbarber <tylerwbarber@gmail.com>
2015-12-23 16:52:09 -0600

    Changed AdultOption field. Added market enum.

--- src/main/java/big/AdultOption.java
+++ src/main/java/big/AdultOption.java
@@ -7,14 +7,14 @@ public enum AdultOption {
 
     OFF("Off"), MODERATE("Moderate"), STRICT("Strict");
 
-    private final String filterLevel;
+    private final String adultOption;
 
-    AdultOption(final String filterLevel) {
-        this.filterLevel = filterLevel;
+    AdultOption(final String adultOption) {
+        this.adultOption = adultOption;
     }
 
     @Override
     public String toString() {
-        return this.filterLevel;
+        return this.adultOption;
     }
 }
---
+++ src/main/java/big/Market.java
@@ -0,0 +1,7 @@
+package big;
+
+/**
+ * Created by Tyler on 12/23/15.
+ */
+public enum Market {
+}
