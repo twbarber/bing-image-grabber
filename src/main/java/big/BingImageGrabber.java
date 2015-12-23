89c4639baa440abad4972c35c2fd1aeb7f0a0db2
twbarber <tylerwbarber@gmail.com>
2015-12-23 17:49:32 -0600

    Added central Bing Image Grabber class to act as main lib interface

---
+++ src/main/java/big/BingImageGrabber.java
@@ -0,0 +1,14 @@
+package big;
+
+/**
+ * Created by Tyler on 12/23/15.
+ */
+public class BingImageGrabber {
+
+  private final Config config;
+
+  public BingImageGrabber(Config config) {
+    this.config = config;
+  }
+
+}
