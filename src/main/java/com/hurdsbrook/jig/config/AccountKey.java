310fcdede6a7f4fe011727d9274d12b6d6ee9225
twbarber <tylerwbarber@gmail.com>
2015-12-24 14:36:31 -0600

    Finished AccountKey. Constructor parameter is encoded on instantiation.

--- src/main/java/com/hurdsbrook/jig/config/AccountKey.java
+++ src/main/java/com/hurdsbrook/jig/config/AccountKey.java
@@ -1,7 +1,20 @@
 package com.hurdsbrook.jig.config;
 
+import org.apache.commons.codec.binary.Base64;
+
 /**
  * Created by Tyler on 12/24/15.
  */
 public class AccountKey {
+
+  private final byte[] encondedKey;
+
+  public AccountKey(String accountKey) {
+    this.encondedKey = Base64.encodeBase64(accountKey.getBytes());
+  }
+
+  public byte[] getEncondedKey() {
+    return this.encondedKey;
+  }
+
 }
