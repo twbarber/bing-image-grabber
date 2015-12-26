470aa89d4e0fd52b3d7468977a80c176cf3b9977
twbarber <tylerwbarber@gmail.com>
2015-12-26 11:10:54 -0500

    Created ImageWriter for saving images to disk

---
+++ src/main/java/jig/bing/ImageWriter.java
@@ -0,0 +1,31 @@
+package jig.bing;
+
+import java.io.File;
+
+/**
+ * Created by Tyler on 12/26/15.
+ */
+public class ImageWriter {
+
+  /**
+   * Creates directories in <user_home>/jig/images/<query_term> where images will be
+   * saved
+   *
+   * @return Directory where images where be saved.
+   */
+  public File makeDirectories() {
+    String userHome = System.getProperty("user.home");
+    File imageDirectory = new File(userHome + "/grabber/images");
+    File queryDirectory = new File(imageDirectory + "/");
+
+    if (!imageDirectory.exists()) {
+      if (imageDirectory.mkdir())
+        System.out.println("\nBing Directory: " + imageDirectory.toString() + " created");
+    }
+    if (!queryDirectory.exists()) {
+      if (queryDirectory.mkdir())
+        System.out.println("\nImageRequest Directory: " + queryDirectory.toString() + " created");
+    }
+    return queryDirectory;
+  }
+}
