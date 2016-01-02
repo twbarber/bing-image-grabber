2519d461c5835f1f9acebef322457e4998d8d106
Tyler Barber <tylerwbarber@gmail.com>
2016-01-02 12:18:08 -0600

    Removed outdated docs from Bing Service

--- src/main/java/jig/bing/BingService.java
+++ src/main/java/jig/bing/BingService.java
@@ -1,36 +1,4 @@
 package jig.bing;
-/**
- * imageDownloader class that handles the actual retrieval of images.
- 	
- 	The imageDownloader is passed in a serialGrabber object, and it uses
- 	the fields to populate:
- 	
- 		- The array of parsed URLs
- 		- The raw query term for directory organization
- 		
- 	The user enters where they want to save the images, and then the images 
- 	are downloaded from the array of URLs. 
- 	
- 	The first time this program saves images to a directory, it will create a 
- 	folder with a name dictated by the user. Each time a new query stores it's 
- 	result in the same folder, sub-folders are created starting with the first query, 
- 	named after the query itself:
- 	
- 		Example: ImageRequest - 456123
- 				 A folder inside the the users created directory named "456123" 
- 				 will be created, and the images resulting form that search will
- 				 be saved there.
- 	
- 	Images are saved with the names:
- 	
- 		"img"  + arrayIndex
- 		Example: img2 for array imageURLs[2];
- 	
- 	After a directory is created, URL connection is opened for URL in array.
- 	Image is read in 2Kb at a time, an file is built.
- 	
- 	Download time varied depending on the size of the image.
- */
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
